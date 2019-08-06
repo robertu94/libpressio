1dd437363cfa03c50a896a120c4758889f9f9749
Robert Underwood <rr.underwood94@gmail.com>
2019-08-06 13:47:48 -0500

    libpressio version 0.6.2
    Bug fix: libpressio previously used the wrong include markers (<>) when
    installed as a system library.  It now uses the correct markers ("").

--- include/libpressio.h
+++ include/libpressio.h
@@ -1,10 +1,10 @@
 /*! \file
  *  \brief A convenience header for libpressio for use in downstream applications
  */
-#include <pressio.h>
-#include <pressio_compressor.h>
-#include <pressio_options.h>
-#include <pressio_options_iter.h>
-#include <pressio_option.h>
-#include <pressio_data.h>
-#include <pressio_metrics.h>
+#include "pressio.h"
+#include "pressio_compressor.h"
+#include "pressio_options.h"
+#include "pressio_options_iter.h"
+#include "pressio_option.h"
+#include "pressio_data.h"
+#include "pressio_metrics.h"
