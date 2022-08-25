5cdc07ec0f3af27ced813c57ec6e43bee366e0f0
Robert Underwood <runderwood@anl.gov>
2022-08-25 10:20:37 -0400

    libpressio version 0.86.4
    Major Changes
    
    + new clipping metric counts the number of values that exceed the
      absolute error bound
    
    Bug fixes
    
    + fixed some cases where linking to HDF5 would fail to pick up some
      dependencies.

---
+++ src/plugins/metrics/clipping.cc
@@ -0,0 +1,90 @@
+#include "pressio_data.h"
+#include "pressio_compressor.h"
+#include "pressio_options.h"
+#include "libpressio_ext/cpp/metrics.h"
+#include "libpressio_ext/cpp/pressio.h"
+#include "libpressio_ext/cpp/options.h"
+#include "std_compat/memory.h"
+
+namespace libpressio { namespace clipping_metrics_ns {
+
+class clipping_plugin : public libpressio_metrics_plugin {
+  public:
+    int end_compress_impl(struct pressio_data const* input, pressio_data const*, int) override {
+      this->input = pressio_data::clone(*input);
+      return 0;
+    }
+
+    struct clipping_op {
+      template <class T, class U>
+      uint64_t operator()(T const* in_begin, T const* in_end, U const* out_begin, U const* out_end) {
+        const size_t n = std::min(
+            std::distance(in_begin, in_end),
+            std::distance(out_begin, out_end)
+            );
+        size_t count = 0;
+        for (size_t i = 0; i < n; ++i) {
+          double diff = std::abs(static_cast<double>(in_begin[i]) - static_cast<double>(out_begin[i]));
+          if (diff > config->abs_bound) {
+            count++;
+          }
+        }
+        return count;
+      }
+      clipping_plugin const* config;
+    };
+
+    int end_decompress_impl(struct pressio_data const* , pressio_data const* output, int) override {
+      clips = pressio_data_for_each<uint64_t>(input, *output, clipping_op{this});
+      return 0;
+    }
+
+  struct pressio_options get_options() const override {
+    pressio_options options;
+    set(options, "pressio:abs", abs_bound);
+    set(options, "clipping:abs", abs_bound);
+    return options;
+  }
+
+  int set_options(pressio_options const& options) override {
+    get(options, "pressio:abs", &abs_bound);
+    get(options, "clipping:abs", &abs_bound);
+    return 0;
+  }
+  
+  
+  struct pressio_options get_configuration() const override {
+    pressio_options opts;
+    set(opts, "pressio:stability", "stable");
+    set(opts, "pressio:thread_safe", static_cast<int32_t>(pressio_thread_safety_multiple));
+    return opts;
+  }
+
+  struct pressio_options get_documentation_impl() const override {
+    pressio_options opt;
+    set(opt, "pressio:description", "measures the number of values that exceed an absolute threshold");
+    set(opt, "clipping:abs", "threshold for error");
+    return opt;
+  }
+
+  pressio_options get_metrics_results(pressio_options const &) override {
+    pressio_options opt;
+    set(opt, "clipping:clips", clips);
+    return opt;
+  }
+
+  std::unique_ptr<libpressio_metrics_plugin> clone() override {
+    return compat::make_unique<clipping_plugin>(*this);
+  }
+  const char* prefix() const override {
+    return "clipping";
+  }
+
+  private:
+  compat::optional<uint64_t> clips = 0;
+  double abs_bound = 1e-4;
+  pressio_data input;
+};
+
+static pressio_register metrics_clipping_plugin(metrics_plugins(), "clipping", [](){ return compat::make_unique<clipping_plugin>(); });
+}}
